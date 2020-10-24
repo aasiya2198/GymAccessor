de9165df7c384b99ade1e6f0bd063a163f724c74
aasiya2198 <61992263+aasiya2198@users.noreply.github.com>
2020-10-23 19:28:16 -0600

    setup method run with log

--- src/Host.java
+++ src/Host.java
@@ -3,4 +3,12 @@ import jade.core.Agent;
 public class Host extends Agent
 {
 
+	@Override
+	protected void setup() {
+		// TODO Auto-generated method stub
+		super.setup();
+		System.out.println("Congratulation the first AGent");
+	}
+	 
+	
 }
