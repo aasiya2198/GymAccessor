20d33e756aa36319ef45d34107711c1c75457eaa
aasiya2198 <61992263+aasiya2198@users.noreply.github.com>
2020-12-11 17:26:36 -0700

    Update UserAgent.java

--- src/UserAgent.java
+++ src/UserAgent.java
@@ -16,8 +16,8 @@ import jade.lang.acl.UnreadableException;
 
 public class UserAgent extends Agent 
 {
-
-ArrayList<Slot> slotInformation = new ArrayList();
+	
+	ArrayList<Slot> slotInformation = new ArrayList();
 	UserUI m_frame = null;
 	
 	Slot userSlot = new Slot();
@@ -103,8 +103,213 @@ ArrayList<Slot> slotInformation = new ArrayList();
 			
 		}
 	}
+	
+	private class GetAdmitterAgent extends Behaviour
+	{
+		boolean isDone = false;
+		
+		@Override
+		public boolean done() {
+			// TODO Auto-generated method stub
+			//addBehaviour(new UserRegisterBehaviour());
+			return isDone;
+		}
+		
+		@Override
+		public void action() {
 
+			DFAgentDescription template = new DFAgentDescription();
+			ServiceDescription sd = new ServiceDescription();
+			sd.setType("AdmitterAgent");
+			template.addServices(sd);
+			
+			DFAgentDescription[] result = null;
+			try {
+				result = DFService.search(myAgent, template);
+			} catch (FIPAException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(result.length >= 1) {
+				//TODO: we will do the put the random agent to assigned here
+				
+				Random rand = new Random();
+				int randIndex = rand.nextInt(result.length);
+				admitterAgent = result[randIndex].getName();
+				//isDone = true;
+		}
+			
+		}
+	}
+	
+	private class UserAdmitterBehaviour extends Behaviour
+	{
+		private int step = 0;
+		boolean isDone = false;
+		@Override
+		public void action() {
+			MessageTemplate mt = null;
+			switch(step)
+			{
+			case 0:
+				//After you get the slot you will send the detail to the RegisterAgent
+				ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+				cfp.addReceiver(admitterAgent);
+				UserDetail user = new UserDetail();
+				user.userId = getAID();
+				user.slotNumber = enteringSlotNumber;
+				try {
+					cfp.setContentObject(user);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				cfp.setConversationId("User-Admitter");
+				myAgent.send(cfp);
+				mt = MessageTemplate.and(MessageTemplate.MatchConversationId("User-Admitter"),
+						MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+				step = 1;
+				break;
+			case 1:
+				ACLMessage reply = myAgent.receive(MessageTemplate.MatchPerformative(ACLMessage.CFP));
+				if(reply != null)
+				{
+					System.out.println("Result : "+reply.getContent());
+					step = -1;
+					isDone = true;
+					if(reply.getContent().contains("Hurray"))
+					{
+						myAgent.doDelete();
+						m_frame.setVisible(false);
+					}
+				}
+				break;
+			}
+			
+		}
 
+		@Override
+		public boolean done() {
+			return isDone;
+		}
+		
+	}
+	
+	private class UserRegisterBehaviour extends Behaviour
+	{
+		private int step = 0;
+		MessageTemplate mt = null;
+		boolean isDone = false;
+		@Override
+		public void action() {
+			
+			switch(step)
+			{
+			case 0:
+				//System.out.println("URB - 0");
+				ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+				if(registerAgent == null)
+					System.out.println("register AGent is null");
+				cfp.addReceiver(registerAgent);
+				UserDetail user = new UserDetail();
+				user.userId = getAID();
+				user.slotNumber = bookingSlotNumber;
+				try {
+					cfp.setContentObject(user);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				cfp.setConversationId("User-Register");
+				myAgent.send(cfp);
+				mt = MessageTemplate.and(MessageTemplate.MatchConversationId("User-Register"),
+						MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+				step = 1;
+				break;
+			case 1:
+				//System.out.println("URB - 1");
+				ACLMessage reply = myAgent.receive(MessageTemplate.MatchPerformative(ACLMessage.CFP));
+				if(reply != null)
+				{
+					System.out.println("Result : "+reply.getContent().toString());
+					step = -1;
+					isDone = true;
+					
+					if(reply.getContent().contains("Success"))
+					{
+						m_frame.DisableBookingButton();
+					}
+					
+				}
+				break;
+			}
+			
+		}
 
+		@Override
+		public boolean done() {
+			// TODO Auto-generated method stub
+			//System.out.println("successfully behaviour is finish");
+			//removeBehaviour(this);
+			return isDone;
+		}
+		
+	}
+	
+	
+	private class GetSlotBehaviour extends Behaviour
+	{
+		boolean isDone = false;
+		int step = 0;
+		MessageTemplate messageTemplate = null;
+		@Override
+		public void action() 
+		{
+			
+			switch(step)
+			{
+			case 0:
+				//System.out.println("requesting slot info");
+				MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+				ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+				msg.addReceiver(registerAgent);
+				msg.setConversationId("slot-info");
+				myAgent.send(msg);
+				messageTemplate = MessageTemplate.and(MessageTemplate.MatchConversationId("slot-info"),
+						MessageTemplate.MatchInReplyTo(msg.getReplyWith()));
+				
+				step = 1;
+				break;
+			case 1:
+				MessageTemplate mt1 = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+				ACLMessage rply = myAgent.receive(mt1);
+				
+				if(rply != null && rply.getConversationId().equals("slot-info"))
+				{
+					
+					//System.out.println("got the reply of slot-info");
+					try {
+						slotInformation = (ArrayList)rply.getContentObject();
+					} catch (UnreadableException e) {
+						e.printStackTrace();
+					}
+					
+					//System.out.println("Size is : "+slotInformation.size());
+					step = -1;
+					setupUI();
+					isDone = true;
+				}
+				
+				
+				break;
+			}			
+			
+		}		
+
+		
+		@Override
+		public boolean done() {
+			return isDone;
+		}
+	}
 
 }
